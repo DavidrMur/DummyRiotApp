2ef3726840c41a94f41862d6a74e4c7bebcb4046
david.murga <davidrmurga@gmail.com>
2019-01-13 13:44:22 -0500

    commit

--- src/axios-config.js
+++ src/axios-config.js
@@ -2,12 +2,6 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'https://na1.api.riotgames.com/',
-    /*headers: {
-        'X-Riot-Token': 'RGAPI-148648a6-fdbe-4ee4-9db6-3e19937b2eca',
-        'Access-Control-Allow-Origin':'http://localhost:3000',
-    }*/
-
-
 });
 
 export default instance;
